7d4b0b66e6a89a9a60f0d84040663117cb20d73f
lewis-otwori <lewisotwori@gmail.com>
2023-03-27 22:18:37 +0300

    Completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+function submitData(name, email) {
+    const info = {
+      name: name,
+      email: email,
+    };
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(info),
+    };
+  
+    return fetch("http://localhost:3000/users", configurationObject)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then(function (data) {
+        let newId = data.id;
+        let idText = document.createTextNode(newId);
+        document.body.appendChild(idText);
+      })
+      .catch((error) => {
+        console.error("An error occurred:", error);
+        let textError = document.createTextNode(error.message);
+        document.body.appendChild(textError);
+      });
+  }
+  
